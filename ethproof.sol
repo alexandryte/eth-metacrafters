bee7c1aa46c797f19934e5c5b37c70d7083a0cfe
alexandryte <75197322+alexandryte@users.noreply.github.com>
2023-09-24 19:43:16 +0800

    Update ethproof.sol

--- ethproof.sol
+++ ethproof.sol
@@ -1,29 +1,28 @@
 // SPDX-License-Identifier: MIT
+// Jewel Anne Panela - BSCSSE
+
 pragma solidity 0.8.18;
 
 contract MyToken {
-    string public tokenName; string public tokenAbbrv;
-    uint256 public totalSupply;
+    string public tokenName = "Spike";
+    string public tokenAbbrv = "SPK";
+    uint256 public totalSupply = 300;
 
-    mapping(address => uint256) public bal;
+    mapping(address => uint256) public balances;
 
-    constructor(string memory _name, string memory _abbrv, uint256 _initialSupply) {
-        tokenName = _name;
-        tokenAbbrv = _abbrv;
-        totalSupply = _initialSupply;
-        bal[msg.sender] = _initialSupply;
+    constructor() {
+        balances[msg.sender] = totalSupply;
     }
 
     function mint(address _to, uint256 _value) public {
         require(_to != address(0), "Invalid address");
         totalSupply += _value;
-        bal[_to] += _value;
+        balances[_to] += _value;
     }
 
-    function burn(address _from, uint256 _value) public {
-        require(_from != address(0), "Invalid address");
-        require(bal[_from] >= _value, "Insufficient balance");
+    function burn(uint256 _value) public {
+        require(balances[msg.sender] >= _value, "Insufficient balance");
         totalSupply -= _value;
-        bal[_from] -= _value;
+        balances[msg.sender] -= _value;
     }
 }
